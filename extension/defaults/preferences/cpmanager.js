b4a5848545bfb842d9bbf4998b94f3902040a161
pzhang <pzhang@mozilla.com>
2011-01-11 10:18:19 +0800

    1. 增加白名单firefox.com.cn 以及 mozilla.cn
    Signed-off-by: pzhang <pzhang@mozilla.com>

--- extension/defaults/preferences/cpmanager.js
+++ extension/defaults/preferences/cpmanager.js
@@ -1,4 +1,4 @@
 pref("browser.tabs.autoHide", false);
 pref("browser.download.useDownloadDir", false);
 
-pref("xpinstall.whitelist.add", "addons.mozilla.org,17huohu.cn,17huohu.com,g-fox.cn,mozilla.com.cn");
+pref("xpinstall.whitelist.add", "addons.mozilla.org,17huohu.cn,17huohu.com,g-fox.cn,mozilla.com.cn,firefox.com.cn,mozilla.cn");
