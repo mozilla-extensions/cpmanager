bd50bdb2f544ff69325a562df941578fe8211663
xxie <xxie@mozilla.com>
2011-01-14 14:10:54 +0800

    Compatibility of both 3.6 and 4.0 beta
    Signed-off-by: xxie <xxie@mozilla.com>

--- xpcom/comp-module.cpp
+++ xpcom/comp-module.cpp
@@ -1,17 +1,43 @@
-#include "nsIGenericFactory.h"
+#include "mozilla/ModuleUtils.h"
 #include "comp-impl.h"
 
+// copy from mozilla/xpcom/sample/nsSampleModule.cpp
 NS_GENERIC_FACTORY_CONSTRUCTOR(CUidGenerator)
-
-static nsModuleComponentInfo components[] =
-{
-    {
-       UIDGENERATOR_CLASSNAME, 
-       UIDGENERATOR_CID,
-       UIDGENERATOR_CONTRACTID,
-       CUidGeneratorConstructor,
-    }
+	NS_DEFINE_NAMED_CID(UIDGENERATOR_CID);
+static const mozilla::Module::CIDEntry kUIDGENERATORCIDs[] = {
+	{ &kUIDGENERATOR_CID, false, NULL,CUidGeneratorConstructor  },
+	{ NULL } 
 };
 
-NS_IMPL_NSGETMODULE("UidGeneratorModule", components) 
+static const mozilla::Module::ContractIDEntry kUIDGENERATORContracts[] = {
+	{ UIDGENERATOR_CONTRACTID, &kUIDGENERATOR_CID },
+	{ NULL }
+};
+
+
+static const mozilla::Module::CategoryEntry kUIDGENERATORCategories[] = {
+	{ "my-category", "my-key", UIDGENERATOR_CONTRACTID },
+	{ NULL }
+};
+
+static const mozilla::Module kUIDGENERATORModule = {
+	mozilla::Module::kVersion,
+	kUIDGENERATORCIDs,
+	kUIDGENERATORContracts,
+	kUIDGENERATORCategories
+};
+
+/*static nsModuleComponentInfo components[] =
+{
+	{
+		UIDGENERATOR_CLASSNAME, 
+			UIDGENERATOR_CID,
+			UIDGENERATOR_CONTRACTID,
+			CUidGeneratorConstructor,
+	}
+};*/
+
+//NS_IMPL_NSGETMODULE("UidGeneratorModule", components) 
+NSMODULE_DEFN(UIDGENERATORModule) = &kUIDGENERATORModule;
+NS_IMPL_MOZILLA192_NSGETMODULE(&kUIDGENERATORModule)
 
