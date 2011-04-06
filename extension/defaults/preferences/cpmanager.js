5e4543729f462d6db6740f2103e5a0e978db4c3d
pzhang <pzhang@mozilla.com>
2011-04-06 17:33:07 +0800

    1. fix bug: firefox can not save download directory . 2. update version to 0.5.1
    Signed-off-by: pzhang <pzhang@mozilla.com>

--- extension/defaults/preferences/cpmanager.js
+++ extension/defaults/preferences/cpmanager.js
@@ -1,4 +1,2 @@
 pref("browser.tabs.autoHide", false);
-pref("browser.download.useDownloadDir", false);
-
 pref("xpinstall.whitelist.add", "addons.mozilla.org,17huohu.cn,17huohu.com,g-fox.cn,mozilla.com.cn,firefox.com.cn,mozilla.cn");
