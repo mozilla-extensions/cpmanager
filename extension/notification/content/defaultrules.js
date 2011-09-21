feddf9300e78749e3e19dd66e9da56f7072088de
Hector Zhao <bzhao@mozilla.com>
2011-09-21 16:47:04 +0800

    fix the video fid

--- extension/notification/content/defaultrules.js
+++ extension/notification/content/defaultrules.js
@@ -6,7 +6,7 @@
 			"btn_id":"star-button",
 			"type":"function",
 			"dest":"tip",
-			"times":1,"desc":"收藏网页？点这里！<br />一键收藏当前网页，Ctrl+B则可打开书签侧栏、对已经收藏的网页进行整理（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=1\">这里</a>）<br/& gt;"
+			"times":1,"desc":"收藏网页？点这里！<br />一键收藏当前网页，Ctrl+B则可打开书签侧栏、对已经收藏的网页进行整理（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=2\">这里</a>）<br/& gt;"
 		}, {
 			"addon_id":"livemargins@mozillaonline.com",
 			"btn_id":"appcenter-button",
@@ -24,19 +24,19 @@
 			"btn_id":"favpart-button",
 			"type":"function",
 			"dest":"tip",
-			"times":1,"desc":"剪下网页中喜欢的部分？点这里！<br />新闻头条、股票走势、日程表、网页游戏、淘宝店铺。。。只有想不到，没有剪不到（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=0\">这里</a>）<br />"
+			"times":1,"desc":"剪下网页中喜欢的部分？点这里！<br />新闻头条、股票走势、日程表、网页游戏、淘宝店铺。。。只有想不到，没有剪不到（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=1\">这里</a>）<br />"
 		}, {
 			"addon_id":"personas@christopher.beard",
 			"btn_id":"personas-selector-button",
 			"type":"function",
 			"dest":"tip",
-			"times":1,"desc":"更换皮肤？点这里！<br />立即给你的浏览器挑选皮肤，实时预览、一键生效（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=2\">这里</a>）<br/& gt;"
+			"times":1,"desc":"更换皮肤？点这里！<br />立即给你的浏览器挑选皮肤，实时预览、一键生效（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=3\">这里</a>）<br/& gt;"
 		}, {
 			"addon_id":"browser",
 			"btn_id":"quicklaunch-button",
 			"type":"function",
 			"dest":"tip",
-			"times":1,"desc":"快捷调用？点这里！<br/>一键打开记事本、计算器、画图等系统程序，还有截图等常用操作（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=5\">这里</a>）<br/& gt;"
+			"times":1,"desc":"快捷调用？点这里！<br/>一键打开记事本、计算器、画图等系统程序，还有截图等常用操作（看详细视频介绍点<a href=\"http://www.firefox.com.cn/video/?fid=6\">这里</a>）<br/& gt;"
 		}],
 		"rules":[]
 	};
