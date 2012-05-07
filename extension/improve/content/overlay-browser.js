36e340c03b09be78443228a74ad7bd80348577db
Frank Jiao <fjiao@mozilla.com>
2012-05-07 14:21:53 +0800

    No Bug: reorganize improve code

--- extension/improve/content/overlay-browser.js
+++ extension/improve/content/overlay-browser.js
@@ -1,4 +1,5 @@
 (function() {
+  //common
 var cmImprove = {
   el: function(id) {
     if (typeof id == 'string')
@@ -6,80 +7,19 @@ var cmImprove = {
     else
       return id;
   },
-  get bookmarksPopup() {
-    delete this.bookmarksPopup;
-    return this.bookmarksPopup = this.el("BMB_bookmarksPopup");
-  },
-
   get _bundles() {
     delete this._bundles;
     return this._bundles = Cc["@mozilla.org/intl/stringbundle;1"].
         getService(Ci.nsIStringBundleService).
         createBundle("chrome://cmimprove/locale/browser.properties");
   },
+}
+  //relating to cert error
+var cmImprove_CE = {
   get _vc() {
     delete this._vc;
     return this._vc = Cc['@mozilla.org/xpcom/version-comparator;1'].
         createInstance(Ci.nsIVersionComparator);
-  },
-  bookmarksPopup_popupshowing : function() {
-    var item_t = cmImprove.el("BMB_viewBookmarksToolbar");
-    item_t && item_t.setAttribute("label",cmImprove._bundles.GetStringFromName("menu.bookmarksToolbar"));
-    var item_s = cmImprove.el("cm_menu_bookmarksSidebar");
-    item_s && item_s.setAttribute("label",cmImprove._bundles.GetStringFromName("menu.bookmarksSidebar"));
-  },
-  showBookmarkToolbar : function() {
-    // If pref "initialized" has been set to True, this means it's not a new profile.
-    var prefs = Application.prefs;
-    if (prefs.getValue("extensions.cpmanager@mozillaonline.com.initialized", false)) {
-      return;
-    }
-
-    if (!prefs.getValue("extensions.cpmanager@mozillaonline.com.show_bookmark_toolbar", false)) {
-      return;
-    }
-
-    prefs.setValue("extensions.cpmanager@mozillaonline.com.show_bookmark_toolbar", false);
-    // Show bookmark toolbar
-    // Only affect FF4.0 or newer version
-    if (window.setToolbarVisibility && document.getElementById("PersonalToolbar")) {
-               setToolbarVisibility(document.getElementById("PersonalToolbar"), true);
-    }
-  },
-  init : function(){
-    PlacesStarButton.onClick = function (aEvent){
-      var tfID = Application.prefs.getValue("extensions.cmimprove.bookmarks.parentFolder",PlacesUtils.unfiledBookmarksFolderId);
-      var showUI = (this._itemIds.length > 0) || Application.prefs.getValue("extensions.cmimprove.bookmarks.add.showEditUI",false);
-      if (aEvent.button == 0 && !this._pendingStmt) {
-        PlacesCommandHook.bookmarkCurrentPage(showUI,tfID);
-      }
-      aEvent.stopPropagation();
-    }
-    PlacesStarButton.__defineGetter__("_unstarredTooltip", function(){
-      delete this._unstarredTooltip;
-      return this._unstarredTooltip =
-        cmImprove._bundles.GetStringFromName("starButtonOff.tooltip");
-    });
-
-    StarUI.panel.addEventListener("popupshown", function () {
-      StarUI._element("editBookmarkPanelTitle").value = cmImprove._bundles.GetStringFromName("editBookmarkPanel.addBookmarkTitle");
-      var footer = document.getAnonymousElementByAttribute(StarUI.panel, "class", "panel-inner-arrowcontentfooter");
-      var link = document.getAnonymousElementByAttribute(footer, "anonid", "promo-link");
-      link.setAttribute("href", "http://www.firefox.com.cn/sync/");
-    },false);
-
-    cmImprove.bookmarksPopup && cmImprove.bookmarksPopup.addEventListener("popupshowing",cmImprove.bookmarksPopup_popupshowing,false);
-    if (cmImprove._vc.compare(Application.version, '11.0') >= 0) {
-      document.getElementById('appcontent').addEventListener('DOMContentLoaded', cmImprove.iFrameCertFix, false);
-    }
-
-    cmImprove.showBookmarkToolbar();
-  },
-  uninit : function(){
-    cmImprove.bookmarksPopup && cmImprove.bookmarksPopup.removeEventListener("popupshowing",cmImprove.bookmarksPopup_popupshowing,false);
-    if (cmImprove._vc.compare(Application.version, '11.0') >= 0) {
-      document.getElementById('appcontent').removeEventListener('DOMContentLoaded', cmImprove.iFrameCertFix, false);
-    }
   },
   iFrameCertFix: function(evt) {
     var contentDoc = evt.target;
@@ -117,9 +57,81 @@ var cmImprove = {
         contentDoc.querySelector('#expertContent').removeAttribute('hidden');
       }
     }
-  }
+  },
+  init : function(){
+    if (cmImprove_CE._vc.compare(Application.version, '11.0') >= 0) {
+      cmImprove.el('appcontent').addEventListener('DOMContentLoaded', cmImprove_CE.iFrameCertFix, false);
+    }
+  },
+  uninit : function(){
+    if (cmImprove_CE._vc.compare(Application.version, '11.0') >= 0) {
+      cmImprove.el('appcontent').removeEventListener('DOMContentLoaded', cmImprove_CE.iFrameCertFix, false);
+    }
+  },
+}
+  //relating to bookmark
+var cmImprove_BM = {
+  get bookmarksPopup() {
+    delete this.bookmarksPopup;
+    return this.bookmarksPopup = cmImprove.el("BMB_bookmarksPopup");
+  },
+  bookmarksPopup_popupshowing : function() {
+    var item_t = cmImprove.el("BMB_viewBookmarksToolbar");
+    item_t && item_t.setAttribute("label",cmImprove._bundles.GetStringFromName("menu.bookmarksToolbar"));
+    var item_s = cmImprove.el("cm_menu_bookmarksSidebar");
+    item_s && item_s.setAttribute("label",cmImprove._bundles.GetStringFromName("menu.bookmarksSidebar"));
+  },
+  showBookmarkToolbar : function() {
+    // If pref "initialized" has been set to True, this means it's not a new profile.
+    var prefs = Application.prefs;
+    if (prefs.getValue("extensions.cpmanager@mozillaonline.com.initialized", false)) {
+      return;
+    }
+
+    if (!prefs.getValue("extensions.cpmanager@mozillaonline.com.show_bookmark_toolbar", false)) {
+      return;
+    }
+
+    prefs.setValue("extensions.cpmanager@mozillaonline.com.show_bookmark_toolbar", false);
+    // Show bookmark toolbar
+    // Only affect FF4.0 or newer version
+    if (window.setToolbarVisibility && cmImprove.el("PersonalToolbar")) {
+               setToolbarVisibility(cmImprove.el("PersonalToolbar"), true);
+    }
+  },
+  init : function(){
+    PlacesStarButton.onClick = function (aEvent){
+      var tfID = Application.prefs.getValue("extensions.cmimprove.bookmarks.parentFolder",PlacesUtils.unfiledBookmarksFolderId);
+      var showUI = (this._itemIds.length > 0) || Application.prefs.getValue("extensions.cmimprove.bookmarks.add.showEditUI",false);
+      if (aEvent.button == 0 && !this._pendingStmt) {
+        PlacesCommandHook.bookmarkCurrentPage(showUI,tfID);
+      }
+      aEvent.stopPropagation();
+    }
+    PlacesStarButton.__defineGetter__("_unstarredTooltip", function(){
+      delete this._unstarredTooltip;
+      return this._unstarredTooltip =
+        cmImprove._bundles.GetStringFromName("starButtonOff.tooltip");
+    });
+
+    StarUI.panel.addEventListener("popupshown", function () {
+      StarUI._element("editBookmarkPanelTitle").value = cmImprove._bundles.GetStringFromName("editBookmarkPanel.addBookmarkTitle");
+      var footer = document.getAnonymousElementByAttribute(StarUI.panel, "class", "panel-inner-arrowcontentfooter");
+      var link = document.getAnonymousElementByAttribute(footer, "anonid", "promo-link");
+      link.setAttribute("href", "http://www.firefox.com.cn/sync/");
+    },false);
+
+    cmImprove_BM.bookmarksPopup && cmImprove_BM.bookmarksPopup.addEventListener("popupshowing",cmImprove_BM.bookmarksPopup_popupshowing,false);
+
+    cmImprove_BM.showBookmarkToolbar();
+  },
+  uninit : function(){
+    cmImprove_BM.bookmarksPopup && cmImprove_BM.bookmarksPopup.removeEventListener("popupshowing",cmImprove_BM.bookmarksPopup_popupshowing,false);
+  },
 }
 
-window.addEventListener('load', cmImprove.init, false)
-window.addEventListener('unload', cmImprove.uninit, false)
+window.addEventListener('load'  , cmImprove_BM.init, false)
+window.addEventListener('unload', cmImprove_BM.uninit, false)
+window.addEventListener('load'  , cmImprove_CE.init, false)
+window.addEventListener('unload', cmImprove_CE.uninit, false)
 })();
