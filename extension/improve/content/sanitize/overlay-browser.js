a0d294b8650e735c620a8df27870ee1880e8ec65
Frank Jiao <fjiao@mozilla.com>
2012-08-07 17:42:52 +0800

    Bug 475/495 followup: toolbar customization

--- extension/improve/content/sanitize/overlay-browser.js
+++ extension/improve/content/sanitize/overlay-browser.js
@@ -2,13 +2,29 @@ var ce_sanitizeHistory = {
   handleEvent: function ce_sanitizeHistory__handleEvent(aEvent) {
     switch (aEvent.type) {
       case "load":
-        this.installButton("ce_sanitizeHistory");
-        this.bindPopup("ce_sanitizeHistory","ce_sanitizeHistory_popup")
+        this.init();
+        break;
+      case "aftercustomization":
+        this.initUI();
         break;
     }
   },
+
+  init: function ce_sanitizeHistory__init(){
+    this.installButton("ce_sanitizeHistory");
+    this.initUI();
+    var toolbox = document.getElementById("navigator-toolbox");
+    toolbox.addEventListener("aftercustomization",this,false)
+  },
+
+  initUI: function ice_sanitizeHistory__initUI(){
+    this.bindPopup("ce_sanitizeHistory","ce_sanitizeHistory_popup")
+  },
+
   bindPopup: function ce_sanitizeHistory__bindPopup(buttonId,menuId){
     var button = document.getElementById(buttonId)
+    if(!button)
+      return;
     var menu = document.getElementById(menuId)
     button.addEventListener("mousedown",function(aEvent){
       if (aEvent.button != 0 )
@@ -16,6 +32,7 @@ var ce_sanitizeHistory = {
       menu.openPopup(button, "before_start", 0, 0, false, false, aEvent);
     },false);
   },
+
   installButton: function ce_sanitizeHistory__installButton(buttonId,toolbarId) {
     toolbarId = toolbarId || "addon-bar";
     var key = "extensions.toolbarbutton.installed."+buttonId;
@@ -39,6 +56,7 @@ var ce_sanitizeHistory = {
     document.persist(toolbar.id, "collapsed");
     Application.prefs.setValue(key, true);
   },
+
   onPopupShowing: function ce_sanitizeHistory__onPopupShowing(){
     var selClose = Application.prefs.getValue("privacy.sanitize.sanitizeOnShutdown",false);
 //    var sel7Days = Application.prefs.getValue("privacy.sanitize.sanitize7Days",false);
@@ -50,6 +68,7 @@ var ce_sanitizeHistory = {
       document.getElementById("ce_sanitizeHistory_none").setAttribute("checked","true");
 
   },
+
   onPopupHiding: function ce_sanitizeHistory__onPopupHiding(){
     var selClose = document.getElementById("ce_sanitizeHistory_onclose").getAttribute("checked") =="true";
 //    var sel7Days = document.getElementById("ce_sanitizeHistory_7days").getAttribute("checked") =="true";
