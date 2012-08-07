fb080c1e9ab0acaf8939630c08d3fba099b61d2b
Frank Jiao <fjiao@mozilla.com>
2012-08-07 12:55:25 +0800

    Bug 496 followup: cmd for mac

--- extension/improve/content/placeholder/overlay-browser.js
+++ extension/improve/content/placeholder/overlay-browser.js
@@ -1,26 +1,35 @@
 (function() {
+Components.utils.import("resource://gre/modules/Services.jsm");
+var isMac = (Services.appinfo.OS == "Darwin");
 var placeholder = {
   handleEvent: function placeholder__handleEvent(aEvent) {
     switch (aEvent.type) {
       case "load":
-        this.init();
+        setTimeout(this.init.bind(this),500);
         break;
     }
   },
   init: function placeholder__init(){
-    var searchbar = document.getElementById("searchbar")
-    if(!searchbar)
-      return;
-    function updateSearchbar(){
-      var name = searchbar.currentEngine.name;
-      searchbar._textbox.placeholder = name + " <Ctrl+K>";
+    var urlbar = document.getElementById("urlbar")
+    if(urlbar){
+      var urlText = urlbar.getAttribute(isMac?"keyLabelMac":"keyLabelNonMac");
+      urlbar.setAttribute("placeholder",urlbar.getAttribute("placeholder")+urlText);
+      
     }
-    var updateDisplay = searchbar.updateDisplay.bind(searchbar);
-    searchbar.updateDisplay = (function() {
-      updateDisplay();
+    var searchbar = document.getElementById("searchbar")
+    if(searchbar){
+      function updateSearchbar(){
+        var searchText = searchbar.getAttribute(isMac?"keyLabelMac":"keyLabelNonMac");
+        var name = searchbar.currentEngine.name;
+        searchbar._textbox.placeholder = name + searchText;
+      }
+      var updateDisplay = searchbar.updateDisplay.bind(searchbar);
+      searchbar.updateDisplay = (function() {
+        updateDisplay();
+        updateSearchbar();
+      }).bind(searchbar);
       updateSearchbar();
-    }).bind(searchbar);
-    updateSearchbar();
+    }
   },
 }
 window.addEventListener('load'  , placeholder, false);
