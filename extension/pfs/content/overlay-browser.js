e3c623469477ddd238a8f4854e144137201488b0
JiaoFeng <fjiao@mozilla.com>
2013-09-26 17:18:45 +0800

    fix bug of embed

--- extension/pfs/content/overlay-browser.js
+++ extension/pfs/content/overlay-browser.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
   var pluginHandler = {
-      handleEvent : function(event) {
+    handleEvent: function(event) {
       var eventType = event.type;
 
       if (eventType == "PluginBindingAttached") {
@@ -26,32 +26,39 @@ window.addEventListener("load", function() {
       return Services.urlFormatter.formatURLPref("plugins.update.url");
     }
   }
-  if(gPluginHandler && gPluginHandler.supportedPlugins){
+  if (gPluginHandler && gPluginHandler.supportedPlugins) {
     gPluginHandler.supportedPlugins.mimetypes["application/qvod-plugin"] = "qvod";
     gPluginHandler.supportedPlugins.plugins["qvod"] = {
       "displayName": "Qvod plugin",
       "installWINNT": true,
     };
   }
-  if (gPluginHandler) {
+  if (gPluginHandler && gPluginHandler.openPluginUpdatePage) {
+    var openPluginUpdatePage = gPluginHandler.openPluginUpdatePage;
     gPluginHandler.openPluginUpdatePage = function(aEvent) {
-      var node = aEvent.target;
-      if (node.tagName.toLowerCase() != "object") {
-        node = document.getBindingParent(aEvent.target);
-      }
-      var type = node.getAttribute("type").toLowerCase();
-      var os = Services.appinfo.OS;
-      if (type == "application/x-shockwave-flash" && os == "WINNT") {
-        openUILinkIn(Services.urlFormatter.formatURLPref("plugins.update.flash.WINNT.url"), "current");
-      } else if (type == "application/x-shockwave-flash" && os == "Darwin") {
-        openURL(Services.urlFormatter.formatURLPref("plugins.update.flash.Darwin.url"));
-      } else {
-        openURL(Services.urlFormatter.formatURLPref("plugins.update.url"));
+      try {
+        var node = aEvent.target;
+        if (node.tagName.toLowerCase() != "object" && node.tagName.toLowerCase() != "embed") {
+          node = document.getBindingParent(aEvent.target);
+        }
+        var info = gPluginHandler._getPluginInfo(node)
+        var type = info.mimetype;
+        var os = Services.appinfo.OS;
+        if (type == "application/x-shockwave-flash" && os == "WINNT") {
+          openUILinkIn(Services.urlFormatter.formatURLPref("plugins.update.flash.WINNT.url"), "current");
+        } else if (type == "application/x-shockwave-flash" && os == "Darwin") {
+          openURL(Services.urlFormatter.formatURLPref("plugins.update.flash.Darwin.url"));
+        } else {
+          openPluginUpdatePage(aEvent);
+        }
+      } catch (e) {
+        openPluginUpdatePage(aEvent);
       }
     }
   }
 
-  if(gPluginHandler && gPluginHandler._clickToPlayNotificationEventCallback){
+  if (gPluginHandler && gPluginHandler._clickToPlayNotificationEventCallback
+                     && gPluginHandler._showClickToPlayNotification) {
     var _showClickToPlayNotification = gPluginHandler._showClickToPlayNotification.bind(gPluginHandler);
     gPluginHandler._showClickToPlayNotification = (function(aBrowser, aPrimaryPlugin) {
       _showClickToPlayNotification(aBrowser, aPrimaryPlugin);
@@ -84,4 +91,4 @@ window.addEventListener("load", function() {
       };
     }).bind(gPluginHandler)
   }
-},false);
+}, false);
