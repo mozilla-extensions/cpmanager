a7677e016b7cfe6c51d974fc3e39683cfd88cb87
Hector Zhao <bzhao@mozilla.com>
2014-04-02 09:23:16 +0800

    Bug 1269: tracking rogue redirects

---
+++ extension/components/mozCNGuard.js
@@ -0,0 +1,90 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+let Cu = Components.utils;
+let Cr = Components.results;
+let Ci = Components.interfaces;
+let Cc = Components.classes;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Services",
+  "resource://gre/modules/Services.jsm");
+
+function mozCNGuard() {}
+
+mozCNGuard.prototype = {
+  classID: Components.ID("{06686705-c9e6-464d-b34f-3c4dc2d5b183}"),
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
+
+  // nsIObserver
+  observe: function MCG_observe(aSubject, aTopic, aData) {
+    switch (aTopic) {
+      case "profile-after-change":
+        Services.obs.addObserver(this, "http-on-examine-response", false);
+        Services.obs.addObserver(this, "http-on-examine-cached-response", false);
+        Services.obs.addObserver(this, "http-on-examine-merged-response", false);
+        break;
+      case "http-on-examine-response":
+      case "http-on-examine-cached-response":
+      case "http-on-examine-merged-response":
+        this.dropRogueRedirect(aSubject);
+        break;
+    }
+  },
+
+  dropRogueRedirect: function MCG_dropRogueRedirect(aSubject) {
+    let channel = aSubject;
+    channel.QueryInterface(Ci.nsIHttpChannel);
+
+    let restrictedHosts = {
+      "huohu123.com": "",
+      "i.firefoxchina.cn": "",
+      "i.g-fox.cn": "",
+      "www.huohu123.com": "",
+      "www.mozilla.org": ""
+    };
+
+    if (Object.keys(restrictedHosts).indexOf(channel.originalURI.host) > -1) {
+      if ([301, 302].indexOf(channel.responseStatus) > -1) {
+        let redirectTo = channel.getResponseHeader("Location");
+        redirectTo = Services.io.newURI(redirectTo, null, channel.originalURI);
+
+        if (Object.keys(restrictedHosts).indexOf(redirectTo.host) == -1) {
+          /*
+          let newURI = channel.originalURI.clone();
+          newURI.host = restrictedHosts[newURI.host] || "about:home";
+
+          let webNavigation = channel.notificationCallbacks.
+            getInterface(Ci.nsIWebNavigation);
+          channel.cancel(Cr.NS_BINDING_ABORTED);
+          webNavigation.loadURI(newURI.spec, null, null, null, null);
+          */
+
+          let tracker = Cc["@mozilla.com.cn/tracking;1"];
+          if (tracker && tracker.getService().wrappedJSObject) {
+            let uuid = "NA";
+            let uuidKey = "extensions.cpmanager@mozillaonline.com.uuid";
+            try {
+              uuid = Services.prefs.getCharPref(uuidKey);
+            } catch(e) {}
+
+            let urlTemplate = "http://addons.g-fox.cn/302.gif?r=%RANDOM%" +
+                              "&status=%STATUS%&from=%FROM%&to=%TO%&id=%ID%";
+            let url = urlTemplate.
+              replace("%STATUS%", channel.responseStatus).
+              replace("%FROM%", encodeURIComponent(channel.originalURI.spec)).
+              replace("%TO%", encodeURIComponent(redirectTo.spec)).
+              replace("%ID%", encodeURIComponent(uuid)).
+              replace("%RANDOM%", Math.random());
+
+            tracker.getService().wrappedJSObject.send(url);
+          }
+        }
+      }
+    }
+  }
+};
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([mozCNGuard]);
