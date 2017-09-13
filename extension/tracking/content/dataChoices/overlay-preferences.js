ea8a14c83e40411c4b223942eaa220d6205e5d57
Hector Zhao <bzhao@mozilla.com>
2017-09-13 16:40:40 +0800

    Bug 2515: part 2, compat w new about:preferences

--- extension/tracking/content/dataChoices/overlay-preferences.js
+++ extension/tracking/content/dataChoices/overlay-preferences.js
@@ -2,16 +2,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 (function() {
-  var cpmPrefs = Cu.import("resource://cmtracking/Prefs.jsm", {}).CPManagerPrefs;
-
-  var cmtracking_init = function() {
-    var checkbox = document.getElementById("submitTrackingBox");
-    checkbox.checked = cpmPrefs.prefs.getValue("extensions.cpmanager.tracking.enabled", false);
-    gAdvancedPane._setupLearnMoreLink("extensions.cpmanager.tracking.infoURL", "trackingLearnMore");
+  if (!window.gPrivacyPane) {
+    return;
   }
-  var _init = gAdvancedPane.init.bind(gAdvancedPane);
-  gAdvancedPane.init = (function() {
-    _init();
-    cmtracking_init();
-  }).bind(gAdvancedPane);
+
+  let strings = Services.strings.createBundle("chrome://cmtracking/locale/preferences.properties");
+  let origInitSubmitCrashes = gPrivacyPane.initSubmitCrashes;
+  gPrivacyPane.initSubmitCrashes = (...args) => {
+    let body = document.querySelector("#dataCollectionGroup");
+
+    let hbox = document.createElement("hbox");
+    hbox.setAttribute("align", "center");
+    let checkbox = document.createElement("checkbox");
+    checkbox.setAttribute("preference", "extensions.cpmanager.tracking.enabled");
+    checkbox.setAttribute("label", strings.GetStringFromName("mococnTracking.label"));
+    checkbox.setAttribute("accesskey", strings.GetStringFromName("mococnTracking.accesskey"));
+    hbox.appendChild(checkbox);
+    let label = document.createElement("label");
+    label.id = "mococnTrackingLearnMore";
+    label.classList.add("learnMore");
+    label.classList.add("text-link");
+    label.textContent = strings.GetStringFromName("mococnTrackingLearnMore.label");
+    hbox.appendChild(label);
+    body.appendChild(hbox);
+
+    gPrivacyPane._setupLearnMoreLink("extensions.cpmanager.tracking.infoURL", "mococnTrackingLearnMore");
+
+    return origInitSubmitCrashes.apply(gPrivacyPane, args);
+  };
 })();
