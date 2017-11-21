0738e58122ee20415be46941118fc27f2ed4dc08
Hector Zhao <bzhao@mozilla.com>
2017-11-21 16:32:45 +0800

    Bug 2545: disable tracking protection only once

--- extension/webextension/trackingProtection.js
+++ extension/webextension/trackingProtection.js
@@ -1,6 +1,43 @@
-(function() {
-  // only set once ?
-  browser.privacy.websites.trackingProtectionMode.set({
-    value: "never"
-  });
+(async function() {
+  try {
+    let result = await browser.storage.local.get({
+      trackingProtection: {
+        disabledOnce: false
+      }
+    });
+    if (result.trackingProtection.disabledOnce) {
+      return;
+    }
+  } catch (ex) {
+    console.error(ex);
+  }
+
+  let origVal = await browser.privacy.websites.trackingProtectionMode.get({});
+  switch (origVal.levelOfControl) {
+    case "not_controllable":
+    case "controlled_by_other_extensions":
+    case "controllable_by_this_extension":
+      let modified = await browser.privacy.websites.trackingProtectionMode.set({
+        value: "never"
+      });
+      if (!modified) {
+        console.error(`Failed with levelOfControl: ${origVal.levelOfControl}`);
+        break;
+      }
+      // intentionally no break when modified is true;
+    case "controlled_by_this_extension":
+      try {
+        await browser.storage.local.set({
+          trackingProtection: {
+            disabledOnce: true
+          }
+        });
+      } catch (ex) {
+        console.error(ex);
+      }
+      break;
+    default:
+      console.error(`Unknown levelOfControl: ${origVal.levelOfControl}`);
+      break;
+  }
 })();
