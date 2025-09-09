bfef3223982e58916233b22810d675173f3b01c3
Andrea Marchesini <amarchesini@mozilla.com>
2025-09-09 18:55:00 +0200

    New events

--- extension/bg/url2qr.js
+++ extension/bg/url2qr.js
@@ -1,23 +1,22 @@
-(async function() {
-  async function maybeHide(tab) {
-    if (tab.active) {
-      const settings = await browser.mozillaonline.chinaPackManager.sendLegacyMessage({ type: "initOptions" });
-      if (await browser.pageAction.isShown({tabId: tab.id}) !== settings.url2qr) {
-        if (!settings.url2qr) {
-          browser.pageAction.hide(tab.id)
-        } else {
-          browser.pageAction.show(tab.id)
-        }
-      }
-    }
+let url2qrEnabled = true;
+
+browser.mozillaonline.chinaPackManager.onURL2QRPrefChange.addListener(enabled => prefChanged(enabled));
+browser.mozillaonline.chinaPackManager.url2qrEnabled().then(enabled => prefChanged(enabled));
+browser.tabs.onCreated.addListener((tab) => hideOrShowPageAction(tab.id));
+
+async function prefChanged(enabled) {
+  url2qrEnabled = enabled;
+
+  const tabs = await browser.tabs.query({});
+  for (const tab of tabs) {
+    hideOrShowPageAction(tab.id);
   }
+}
 
-  browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    maybeHide(tab);
-  }, { properties: ["url"] });
-
-  browser.tabs.onActivated.addListener(async (tabInfo) => {
-    const tab = await browser.tabs.get(tabInfo.tabId);
-    maybeHide(tab);
-  });
-})();
+function hideOrShowPageAction(tabId) {
+  if(url2qrEnabled) {
+   browser.pageAction.show(tabId)
+  } else {
+   browser.pageAction.hide(tabId)
+  }
+}
